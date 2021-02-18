06994332e5a72536545bb36a0d0531a596a447ab
7700250090 <phelipe.matos-ext@viavarejo.com.br>
2021-02-18 15:48:26 -0300

    Exportando módulos referente ao 'ErrorHandling'

---
+++ projects/ph-core/src/lib/utils/error-handling/index.ts
@@ -0,0 +1,5 @@
+export * from './error-handling.module';
+export * from './error-handling.service';
+export * from './modal/modal-error-handling.component';
+export * from './model/modal-parameters.model';
+export * from './model/type-error.enum';
