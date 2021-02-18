9c0d014e62af66bcb16bcb83ae373fdb7d19fbed
7700250090 <phelipe.matos-ext@viavarejo.com.br>
2021-02-18 13:57:50 -0300

    Criação dos ENUM responsável por compor o ErrorHandling

---
+++ projects/ph-core/src/lib/utils/error-handling/model/error-message.const.ts
@@ -0,0 +1,8 @@
+export const ErrorMessage = {
+  BAD_REQUEST: 'Falha na requisição, verifique se o endpoint realmente existe.',
+  UNAUTHORIZED: 'Por favor, atualize a página ou acesse novamente a aplicação para autenticar seu usuário.',
+  UNPROCESSABLE_ENTITY: 'Falha na requisição, verifique se os parâmetros do endpoint estão corretos.',
+  INTERNAL_SERVER_ERROR: 'Falha na requisição, verifique se a conexão com rede está ativa.',
+  STANDARD_API: 'Infelizmente ocorreu um erro na requisição do endpoint, tente novamente.',
+  STANDARD_FRONT: 'Infelizmente ocorreu um erro de JavaScript, tente novamente.'
+};
